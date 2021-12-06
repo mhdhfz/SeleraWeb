2f96bc6bde86ab287ac3a8b6eb22c303e1034e59
Hafiz Mustafa <mhd.hfzz@gmail.com>
2021-12-06 14:25:37 +0800

    Add project files.

---
+++ SeleraWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleraWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
